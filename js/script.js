69ffafcdd3f4e8c325e7bbe6c6872fcbf7b34d89
Harsh <harsh.bitz@gmail.com>
2022-05-29 19:17:49 +0530

    Completed until pricing section

--- js/script.js
+++ js/script.js
@@ -50,3 +50,30 @@ navbar.forEach(function(a) {
 })
 
 
+// counter--------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", () =>{
+function counter(id, start, end, duration) {
+  let obj = document.getElementById(id),
+  current = start,
+  range = end - start,
+  increment = end > start ? 1 : -1,
+  step = Math.abs(Math.floor(duration / range)),
+  timer = setInterval(() => {
+    current += increment;
+    obj.textContent = current;
+    if(current == end){
+      clearInterval(timer);
+    }
+  }, step);
+}
+
+counter("count1", 0, 1287, 3000);
+counter("count2", 100, 4500, 2500);
+counter("count3", 0, 1440, 3000);
+counter("count4", 0, 3110, 3000);
+
+
+}); 
+
+
